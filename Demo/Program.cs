2e64cbadbda88155c38587623549d11dd9ba7258
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-11-05 09:23:05 +0530

    [Tirupathi] Add. Welcome message to Linq

---
+++ Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review using Linq");
+            
+        }
+
+
+    }
+}
